c00874451d0f8e0175559771b3a2f1c76513564b
steffanynaranjov <steffany.naranjo@gmail.com>
2021-04-22 21:11:24 -0500

    update comment

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
-""" Unlocking boxes module """
+"""
+This a solution for a lockboxes challenge
+"""
 
 
 def canUnlockAll(boxes):
     """
-    check if all boxes can be unlocked method
-    A key with the same number as a box(index) opens that box
-    Each box is numbered sequentially from 0 to n - 1
-    The first box in boxes [0] is unlocked
-    Return:
-        True - if all boxes can be unlocked otherwise, false
+    Rreturns True if all boxes can be opened or
+    Returns False if they are not
     """
-    keys_stack = [0]
-    locked_box = 0
-    unlocked_box = 1
-    # first boxes index will be always unlocked
-    unlocked_checker = [unlocked_box] + [locked_box] * (len(boxes) - 1)
-    if len(boxes) == 0:
-        return True
-    if not isinstance(boxes, list):
-        return False
-    while keys_stack:
-        index = keys_stack.pop()
-        for key in boxes[index]:
-            if key < len(boxes) and unlocked_checker[key] == locked_box:
-                unlocked_checker[key] = unlocked_box
-                keys_stack.append(key)  # lets check for those keys as index
 
-    if locked_box in unlocked_checker:
-        return False
-    return True
+    keys = [0]
+    unlockedBoxes = list(range(len(boxes)))
+
+    # if there is not more boxes the loops will stop with this parameter
+    lockedBox = True
+
+    while lockedBox:
+        lockedBox = False
+        for x in range(len(boxes)):
+            if (x in keys and x in unlockedBoxes):
+                keys.extend(boxes[x])
+                while(unlockedBoxes.count(x)):
+                    unlockedBoxes.remove(x)
+                lockedBox = True
+    if not unlockedBoxes:
+        return True
+    return False
+    
