4a8d1e5be2cc57e698ccc4182268762580d066cc
steffanynaranjov <steffany.naranjo@gmail.com>
2021-05-06 17:53:31 -0500

    update zero

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -13,9 +13,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_node = NULL;
 
 	new_node = malloc(sizeof(binary_tree_t));
-
 	if (new_node == NULL)
-		return (NULL);
+			return (NULL);
 
 	new_node->n = value;
 	new_node->parent = parent;
